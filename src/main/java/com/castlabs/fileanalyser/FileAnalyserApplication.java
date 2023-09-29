fd347c7ded614f619b1311ebfc42bd6dbdd0d4c0
yosrafatnassi <yosrafatnassi6@gmail.com>
2023-09-29 14:12:18 +0200

    init project

---
+++ src/main/java/com/castlabs/fileanalyser/FileAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.castlabs.fileanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileAnalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castlabs/fileanalyser/FileAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castlabs.fileanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
